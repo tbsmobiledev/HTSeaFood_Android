298a79aa10fbf16949945bf632d7b70878f66898
tec-anDro-DeV <45812628+tec-anDro-DeV@users.noreply.github.com>
2023-08-22 19:05:59 +0530

    - Invoice detail screen design start

--- app/src/main/java/com/example/htseafood/adpter/InvoiceListAdapter.kt
+++ app/src/main/java/com/example/htseafood/adpter/InvoiceListAdapter.kt
@@ -2,10 +2,12 @@ package com.example.htseafood.adpter
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.htseafood.activity.InvoiceDetailActivity
 import com.example.htseafood.databinding.ItemInvoiceBinding
 
 
@@ -31,6 +33,10 @@ class InvoiceListAdapter(
                 holder.binding.llExtraView.visibility = View.GONE
                 holder.binding.ivArrow.rotation = 0f
             }
+        }
+
+        holder.binding.llInvoice.setOnClickListener {
+            mActivity.startActivity(Intent(mActivity, InvoiceDetailActivity::class.java))
         }
 
     }
